d9fba15b7dfba876392133ae45207d4cefaf473d
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-05-23 21:10:33 -0300

    feat: screenshot using puppeteer

--- backend-node/server.js
+++ backend-node/server.js
@@ -10,6 +10,9 @@ server.use(
   })
 );
 
+const puppeteerRoute = require("./Puppeteer");
+server.use("/", puppeteerRoute);
+
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
 });
